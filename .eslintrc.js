d5f74d171ae1fd310bd5fcc29a707a42fe7fda0f
MarwanRadwan7 <marwan.swe@outlook.com>
2024-01-23 12:15:57 +0200

    Add promise checks i linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
+  plugins: ['promise'],
   rules: {
     quotes: [2, 'single', { avoidEscape: true }],
     'no-shadow': 'off',
@@ -19,5 +20,18 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/array-type': 'off',
     '@typescript-eslint/no-object-literal-type-assertion': 'off',
+
+    'promise/always-return': 'error',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'warn',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/avoid-new': 'warn',
+    'promise/no-new-statics': 'error',
+    'promise/no-return-in-finally': 'warn',
+    'promise/valid-params': 'warn',
   },
 };
