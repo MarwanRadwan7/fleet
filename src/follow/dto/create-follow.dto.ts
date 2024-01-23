359e76286d1ece07310b678f7ebf9681a0e345c1
MarwanRadwan7 <marwan.swe@outlook.com>
2024-01-23 12:37:27 +0200

    Add docs for follow

--- src/follow/dto/create-follow.dto.ts
+++ src/follow/dto/create-follow.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateFollowDto {
+  @ApiProperty({ example: 'cc0dee72-7d82-4310-bdd9-b9babb141f87' })
   @IsNotEmpty()
   @IsString()
   friendId: string;
--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -3,13 +3,42 @@ import { Body, Controller, Req, UseGuards, Post, Delete, HttpCode, Param } from 
 import { JwtGuard } from 'src/auth/guard';
 import { FollowService } from './follow.service';
 import { CreateFollowDto, DeleteFollowDto } from './dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiSecurity,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('follow')
+@ApiTags('Follow')
 export class FollowController {
   constructor(private readonly followService: FollowService) {}
 
   @Post('/')
   @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
+  @ApiOperation({ summary: 'Follows a user' })
+  @ApiCreatedResponse({
+    description: 'user followed successfully',
+  })
+  @ApiNotFoundResponse({ description: 'user not fount' })
+  @ApiBadRequestResponse({ description: 'you cannot follow yourself' })
+  @ApiConflictResponse({ description: 'you are blocked. you cannot follow this user' })
+  @ApiConflictResponse({ description: 'user is already followed' })
+  @ApiUnauthorizedResponse({ description: 'unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @ApiBody({
+    required: true,
+    type: CreateFollowDto,
+  })
   async follow(@Req() req, @Body() followBody: CreateFollowDto) {
     await this.followService.follow(req.user.id, followBody);
     return { statusCode: 201, message: 'user followed successfully' };
@@ -18,6 +47,13 @@ export class FollowController {
   @Delete('/:user_id')
   @UseGuards(JwtGuard)
   @HttpCode(204)
+  @ApiSecurity('JWT-auth')
+  @ApiOperation({ summary: 'Unfollow a user' })
+  @ApiNoContentResponse({ description: 'user unfollowed successfully' })
+  @ApiBadRequestResponse({ description: 'you cannot unfollow yourself' })
+  @ApiNotFoundResponse({ description: 'user not found' })
+  @ApiUnauthorizedResponse({ description: 'unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async unFollow(@Req() req, @Param('user_id') followingId: string) {
     const payload: DeleteFollowDto = { user_id: req.user.id, following_id: followingId };
     await this.followService.unFollow(payload);
